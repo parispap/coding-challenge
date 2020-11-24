f8c355d8a34b1243af54034722b1f0376ef023f0
Paris Papadopoulos <parispap1995@gmail.com>
2020-11-24 23:16:36 +0200

    Instruction clean

--- src/pages/game_page/components/insructions.jsx
+++ src/pages/game_page/components/insructions.jsx
@@ -2,31 +2,26 @@ import React from "react";
 import Popup from "reactjs-popup";
 import "reactjs-popup/dist/index.css";
 
-import styles from './styles/instructions.module.css'
+import styles from "./styles/instructions.module.css";
 
 const PopInstructions = () => {
   return (
-    <Popup
-      trigger={<a className={styles.button}> How it Works </a>}
-      modal
-      open
-    >
+    <Popup trigger={<a className={styles.button}> How it Works </a>} modal>
       {(close) => (
         <div className={styles.modal}>
           <div className={styles.header}> Modal Title </div>
           <div className={styles.content}>
+            <h3>How it Works?</h3>
 
-              <h3>How it Works?</h3>
-
-              <p>
-                  In order to play you must place a bet by clicking at the Place your Bet to start button.
-              </p>
+            <p>
+              In order to play you must place a bet by clicking at the Place
+              your Bet to start button.
+            </p>
           </div>
           <div className={styles.actions}>
             <button
               className="button"
               onClick={() => {
-                
                 close();
               }}
             >
